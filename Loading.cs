263b8230768b4cfc43604bd2acf825c48ceaf14b
Hadi HD <70514223+hadi-hd@users.noreply.github.com>
2021-12-28 12:26:33 +0330

    Simple delayed loading script for unity engine

---
+++ Loading.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour {
+
+	public Image loadingIMG;
+	public float load;
+	public int timer;
+
+	void Update () {
+		load = Random.Range (0f,0.3f);
+		timer = Random.Range (0,10);
+		Invoke ("Loader",timer);
+	}
+	void Loader () {
+		loading.fillAmount += load * Time.deltaTime;
+		if (loadingIMG.fillAmount >= 1){
+			SceneManager.LoadScene ("your scene name");
+		}
+	}
+}
